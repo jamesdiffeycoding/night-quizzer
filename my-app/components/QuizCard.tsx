1ad42f712aad18e28a1b4cd45dd79918b6d69152
James Diffey <jamesdiffey8@gmail.com>
2024-07-01 11:50:00 +0100

    typescript debugging 2

--- my-app/components/QuizCard.tsx
+++ my-app/components/QuizCard.tsx
@@ -15,8 +15,8 @@ export default function QuizCard({ fetchedQuizData = [], userId }: { fetchedQuiz
         <>
             <div className="animate-in flex-1 flex flex-col gap-5 opacity-0 px-3 mt-5 mb-5 min-w-fit w-3/4 max-w-screen-sm">
                 {fetchedQuizData.map((quiz) => (
-                    <div key={quiz.id} className={userId == quiz.user_id ?("shadow-md rounded-lg p-4 m-2 border border-purple-300"):("shadow-md rounded-lg p-4 m-2 border border-blue-300")} >
-                        <h2 className="text-xl">
+                    <div key={quiz.id} className={`shadow-md rounded-lg p-4 m-2 ${userId === quiz.user_id ? 'border-purple-300' : 'border-green-400'}`}>
+                    <h2 className="text-xl">
                             <span className="bg-gray-800 rounded-md p-2 text-sm">
                                 {userId == quiz.user_id ?("your quiz"):("public quiz")}  
                             </span>
